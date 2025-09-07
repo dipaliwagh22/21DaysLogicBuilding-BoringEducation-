70919c56898b6044a36d333e8abb4eedf65627a7
dipaliwagh22 <deepaliw121@gmail.com>
2025-09-07 21:52:47 +0530

    Day8

---
+++ Day8.py
@@ -0,0 +1,13 @@
+# input:6
+# output:
+# ******
+# ******
+# ******
+# ******
+# ******
+# ******
+n = int(input("Enter:"))
+for i in range(n):
+    for j in range(n):
+        print("*", end="")
+    print() 
