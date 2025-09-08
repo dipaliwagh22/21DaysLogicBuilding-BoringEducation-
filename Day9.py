2a755178199fc25ff1ef4eefa195d6d12d4eb9cc
dipaliwagh22 <deepaliw121@gmail.com>
2025-09-08 21:21:22 +0530

    Day9

---
+++ Day9.py
@@ -0,0 +1,11 @@
+# write a program to reverse a triangle
+# *****
+# ****
+# ***
+# **
+# *
+n=int(input("Enter:"))
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
